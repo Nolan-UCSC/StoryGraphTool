5edcea8f51da2e68bf81b38512e6eb78fa10401c
Nolan Jensen <130103322+Nolan-UCSC@users.noreply.github.com>
2024-04-16 17:36:51 -0700

    Updated the project for Game 1(b): 👾 Link-based world (implementation)

--- rules.js
+++ rules.js
@@ -1,33 +1,54 @@
 class Start extends Scene {
     create() {
-        this.engine.setTitle("Title goes here"); // TODO: replace this text using this.engine.storyData to find the story title
-        this.engine.addChoice("Begin the story");
+        localStorage.setItem('grabbedLaptop', 'false'); //makes it so the game can be replaued
+        this.engine.setTitle(this.engine.storyData.Title); 
+        this.engine.addChoice(this.engine.storyData.InitialLocation);
     }
 
     handleChoice() {
-        this.engine.gotoScene(Location, "Home"); // TODO: replace this text by the initial location of the story
+        this.engine.gotoScene(Location,this.engine.storyData.InitialLocation);
     }
 }
 
 class Location extends Scene {
     create(key) {
-        let locationData = undefined; // TODO: use `key` to get the data object for the current story location
-        this.engine.show("Body text goes here"); // TODO: replace this text by the Body of the location data
+        console.log(key);
+        let locationData = this.engine.storyData.Locations[key]; 
+        this.engine.show(locationData.Body);
         
-        if(true) { // TODO: check if the location has any Choices
-            for(let choice of ["example data"]) { // TODO: loop over the location's Choices
-                this.engine.addChoice("action text"); // TODO: use the Text of the choice
-                // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
+        if(locationData.Choices) { 
+            for (let choice of locationData.Choices) {
+                if (key === "Unit1") {
+                    // If the laptop has been grabbed aka the KEY, show the "Open your laptop" choice aka the LOCK
+                    if (localStorage.getItem('grabbedLaptop') === 'true' && choice.Nolan) {
+                        this.engine.addChoice(choice.Nolan, { Target: choice.Target });
+                    } // Do not show "You forgot your laptop" if the laptop has been grabbed this way the story is more fun
+                    else if (localStorage.getItem('grabbedLaptop') !== 'true' && choice.Text) {
+                        this.engine.addChoice(choice.Text, choice);
+                    }
+                } else {
+                    this.engine.addChoice(choice.Text, choice);
+                }
             }
         } else {
-            this.engine.addChoice("The end.")
+            this.engine.addChoice("The end.");
         }
     }
 
     handleChoice(choice) {
-        if(choice) {
-            this.engine.show("&gt; "+choice.Text);
+        if (choice) {
+            this.engine.show("> " + choice.Text);
             this.engine.gotoScene(Location, choice.Target);
+
+            // Set a flag in local storage when "Grab the laptop" is chosen
+            if (choice.Text === "Grab the laptop") {
+                localStorage.setItem('grabbedLaptop', 'true');
+            }
+
+            // Set a flag in local storage when visiting "Double Check"
+            if (choice.Target === "Double Check") {
+                localStorage.setItem('visitedDoubleCheck', 'true');
+            }
         } else {
             this.engine.gotoScene(End);
         }
